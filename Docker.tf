9cf25a575ce26f17670ba20403579614a803e718
Ahmad12333 <49105888+Ahmad12333@users.noreply.github.com>
2021-06-08 06:22:24 -0500

    count and path

--- Docker.tf
+++ Docker.tf
@@ -16,64 +16,35 @@ resource "docker_image" "nginx" {
        name = "nginx:1.11-alpine"
 }
 
-resource "docker_container" "nginx-server" {
-   name = "nginx-server"
+resource "docker_container" "nginx-servers" {
+   name = "nginx-server${format("%02d", count.index+1)}"
    image = "${docker_image.nginx.latest}"
+   count = 2
    ports {
    internal = 80
     }
    volumes {
-     container_path = "/usr/share/nginx/html"
-     host_path = "/home/scrapbook/totorial/www"
+     container_path = "/home/terraform/Desktop/Terraform/nginx-container/html"
+     host_path = "/home/terraform/Desktop/Terraform/nginx-servers"
      read_only = true
   }
 }
 
-resource "docker_container" "nginx-server2" {
-   name = "nginx-server2"
-   image = "${docker_image.nginx.latest}"
-   ports {
-   internal = 80
-    }
-   volumes {
-     container_path = "/usr/share/nginx/html"
-     host_path = "/home/Dock"
-     read_only = true
-  }
-}
 
 resource "docker_image" "redis" {
 	name = "redis:latest"
 }
 
-resource "docker_container" "redis-servers" {
-	image = "${docker_image.redis.latest}"
-	name = "redis-${format("%02d", count.index+1)}"
 
-	count = 1
 
-	restart = "always"
-
-	volumes {
-		container_path  = "/usr/local/etc/redis"
-		host_path = "/home/dmportella/_workspaces/terraform/redis-cluster/config/redis"
-		read_only = false
-	}
-
-	command = ["redis-server", "/usr/local/etc/redis/redis.conf"]
-}
-
-resource "docker_container" "haproxy-redis-lb" {
-	depends_on = ["docker_container.redis-servers"]
+resource "docker_container" "haproxy"{
 
 	image = "haproxy:1.5.18"
-	name = "haproxy-redis-lb"
-
+	name = "haproxy"
 	restart = "always"
-
 	volumes {
-		container_path  = "/usr/local/etc/haproxy"
-		host_path = "/home/dmportella/_workspaces/terraform/redis-cluster/config/haproxy"
+		container_path  = "/home/terraform/Desktop/Terraform/haproxy-container"
+		host_path = "/home/terraform/Desktop/Terraform/haproxy"
 		read_only = false
 	}
 }
